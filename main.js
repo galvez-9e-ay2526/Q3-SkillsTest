e9863ffe6cef40eccd6294b6e1a77c7c06d1d97d
galvez-9e-ay2526 <galvez.jaden@obmontessori.edu.ph>
2026-03-15 11:59:25 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+//Variable declaration
+const ADMINFEE = 50.00;
+const QUANTITY = 250.00;
+let quantity = document.getElementById("quantity").value
+let total = subTotal * 250
+let subTotal = quantity * 250;
